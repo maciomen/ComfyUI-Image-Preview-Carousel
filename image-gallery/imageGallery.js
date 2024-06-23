c220b1f6f36e8e04bec0e3321a2912a324f5ad9d
maciomen <you@example.com>
2024-06-23 03:02:31 +0200

    feat: add zoom

--- image-gallery/imageGallery.js
+++ image-gallery/imageGallery.js
@@ -29,10 +29,16 @@ var styles = `
     max-height: 90vh;
     max-width: 90vw;
     margin: auto;
+    transition: transform 0.15s ease, transform-origin 0.01s ease; /* Smooth transition for the zoom effect */
+    cursor: pointer; /* Pointer cursor on hover */
 }
 
 .comfy-carousel-box .slides img.shown {
     display: block;
+}
+
+.comfy-carousel-box .slides img.zoomed {
+    transform: scale(3); /* Zoom level */
 }
 
 .comfy-carousel-box .prev:before,
@@ -101,6 +107,87 @@ app.registerExtension({
   },
   beforeRegisterNodeDef(nodeType, nodeData) {
 	if (nodeData.name === "SaveImage" || nodeData.name === "PreviewImage") {
+      const addZoom = (main) => {
+        let isDragging = false;
+        let startX = 0;
+        let startY = 0;
+        let moveX = 0;
+        let moveY = 0;
+        let lastTranslateX = 0;
+        let lastTranslateY = 0;
+
+        main.addEventListener('mousedown', (event) => {
+          event.preventDefault();
+          let img = main.querySelector('.shown');
+          if (!img.classList.contains('zoomed')) return;
+
+          isDragging = true;
+          const rect = img.getBoundingClientRect();
+          startX = event.clientX - rect.left;
+          startY = event.clientY - rect.top;
+        });
+
+        document.addEventListener('mousemove', (event) => {
+          let img = main.querySelector('.shown');
+
+          const isZoomed = img?.classList.contains('zoomed');
+          if (!isDragging || !isZoomed ) return;
+          event.preventDefault();
+          event.stopPropagation();
+
+          const rect = img.getBoundingClientRect();
+          moveX = (event.clientX - rect.left - startX) * 2;
+          moveY = (event.clientY - rect.top - startY) * 2;
+
+          let currentScale = getCurrentScale(img.style.transform);
+          // Apply the movement as translation, adding to the last translation
+          img.style.transform = `scale(${currentScale}) translate(${lastTranslateX + moveX}px, ${lastTranslateY + moveY}px)`;
+        });
+
+        document.addEventListener('mouseup', (event) => {
+          if (!isDragging) return;
+          isDragging = false;
+
+          lastTranslateX += moveX;
+          lastTranslateY += moveY;
+          moveX = 0;
+          moveY = 0;
+        });
+
+
+        main.addEventListener('click', (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+        });
+
+        main.addEventListener('wheel', (event) => {
+          let img = slides.querySelector('.shown');
+          event.preventDefault(); // Prevent the page from scrolling
+          event.stopPropagation();
+      
+          // Determine the direction of the scroll and adjust the scale accordingly
+          const scaleIncrement = 0.3; // Adjust this value to control the zoom speed
+          const currentScale = getCurrentScale(img.style.transform);
+          let newScale = event.deltaY < 0 ? currentScale + scaleIncrement : currentScale - scaleIncrement;
+      
+          newScale = Math.min(Math.max(newScale, 1), 10);          
+
+          if (newScale > 1) {
+            img.style.transform = `scale(${newScale}) translate(${lastTranslateX}px, ${lastTranslateY}px)`;
+          }
+      
+          // Toggle the 'zoomed' class based on the new scale
+          if (newScale > 1 && !img.classList.contains('zoomed')) {
+            img.classList.add('zoomed');
+          } else if (newScale <= 1 && img.classList.contains('zoomed')) {
+            img.classList.remove('zoomed');
+            img.style.transform = `scale(1) translate(0px, 0px)`;
+            lastTranslateX = 0;
+            lastTranslateY = 0;
+          }
+        });
+      }
+
       const getActive = () => {
         const active = slides.querySelector('.shown');
         const imageIndex = [...slides.childNodes].indexOf(active);
@@ -138,7 +225,9 @@ app.registerExtension({
       const slides = $el("div.slides");
       const dots = $el("div.dots");
       const carousel = $el("div.comfy-carousel-box", { 
-        $: (el) => el.addEventListener('click', (e) => prevSlide(e), true), 
+        $: (el) => {
+          addZoom(el);
+        }, 
       }, [
         slides,
         dots,
@@ -202,4 +291,19 @@ function mergeImages(image1, image2) {
   out.src = dataURL;
 
   return out;
+}
+
+function getCurrentScale(transform) {
+  const match = transform.match(/scale\(([^)]+)\)/);
+  return match ? parseFloat(match[1]) : 1;
+}
+
+function getCurrentTransformOriginX(transform) {
+  const match = transform.match(/transform-origin:\s*([^%]+)%/);
+  return match ? parseFloat(match[1]) : 0;
+}
+
+function getCurrentTransformOriginY(transform) {
+  const match = transform.match(/transform-origin:\s*[^%]+\s+([^%]+)%/);
+  return match ? parseFloat(match[1]) : 0;
 }
